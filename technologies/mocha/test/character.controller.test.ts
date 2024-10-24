531963ad1ce69cb7500958a4940e935fea47ad73
Lucas Pennice <lucaspennice@gmail.com>
2024-10-24 11:28:46 -0300

    añade test dinamicos a mocha

--- technologies/mocha/test/character.controller.test.ts
+++ technologies/mocha/test/character.controller.test.ts
@@ -1,11 +1,11 @@
 import { assert, expect } from "chai";
 import sinon from "sinon";
 import {
-  findOne,
-  add,
-  update,
-  remove,
-  findAll,
+	findOne,
+	add,
+	update,
+	remove,
+	findAll,
 } from "../src/character/character.controller.js";
 import { CharacterRepository } from "../src/character/character.repository.js";
 import { Request, Response, NextFunction } from "express";
@@ -13,230 +13,230 @@ import { Request, Response, NextFunction } from "express";
 sinon.mock("./character.controller");
 
 const mockRequest = () => {
-  const req: Partial<Request> = {
-    body: {},
-    params: {},
-  };
-  return req as Request;
+	const req: Partial<Request> = {
+		body: {},
+		params: {},
+	};
+	return req as Request;
 };
 
 const mockResponse = () => {
-  const res: Partial<Response> = {
-    json: sinon.stub(),
-    status: sinon.stub().returnsThis(),
-    send: sinon.stub().returnsThis(),
-  };
-  return res as Response;
+	const res: Partial<Response> = {
+		json: sinon.stub(),
+		status: sinon.stub().returnsThis(),
+		send: sinon.stub().returnsThis(),
+	};
+	return res as Response;
 };
 
 describe("CharacterController CRUD", function () {
-  let repository: CharacterRepository;
-  let req: Request;
-  let res: Response;
-  let next: NextFunction;
+	let repository: CharacterRepository;
+	let req: Request;
+	let res: Response;
+	let next: NextFunction;
 
-  beforeEach(function () {
-    repository = new CharacterRepository();
-    req = mockRequest();
-    res = mockResponse();
-    next = sinon.spy();
+	beforeEach(function () {
+		repository = new CharacterRepository();
+		req = mockRequest();
+		res = mockResponse();
+		next = sinon.spy();
 
-    sinon.stub(repository, "findAll");
-    sinon.stub(repository, "findOne");
-    sinon.stub(repository, "add");
-    sinon.stub(repository, "update");
-    sinon.stub(repository, "delete");
-  });
+		sinon.stub(repository, "findAll");
+		sinon.stub(repository, "findOne");
+		sinon.stub(repository, "add");
+		sinon.stub(repository, "update");
+		sinon.stub(repository, "delete");
+	});
 
-  afterEach(function () {
-    sinon.restore();
-  });
+	afterEach(function () {
+		sinon.restore();
+	});
 
-  it("should return all characters", () => {
-    repository.findAll();
+	it("should return all characters", () => {
+		repository.findAll();
 
-    findAll(req, res);
-    // @ts-ignore
-    expect(res.json.firstCall.args[0]).to.deep.equal({
-      data: [
-        {
-          name: "Darth Vader",
-          characterClass: "Sith",
-          level: 11,
-          hp: 101,
-          mana: 22,
-          attack: 11,
-          items: ["Lightsaber", "Death Star"],
-          id: "1234",
-        },
-        {
-          name: "Yoda",
-          characterClass: "Jedi",
-          level: 12,
-          hp: 120,
-          mana: 25,
-          attack: 12,
-          items: ["Lightsaber", "Wisdom"],
-          id: "9101",
-        },
-      ],
-    });
-  });
+		findAll(req, res);
+		// @ts-ignore
+		expect(res.json.firstCall.args[0]).to.deep.equal({
+			data: [
+				{
+					name: "Darth Vader",
+					characterClass: "Sith",
+					level: 11,
+					hp: 101,
+					mana: 22,
+					attack: 11,
+					items: ["Lightsaber", "Death Star"],
+					id: "1234",
+				},
+				{
+					name: "Yoda",
+					characterClass: "Jedi",
+					level: 12,
+					hp: 120,
+					mana: 25,
+					attack: 12,
+					items: ["Lightsaber", "Wisdom"],
+					id: "9101",
+				},
+			],
+		});
+	});
 
-  it("should return a character by id", function () {
-    req.params.id = "1234";
-    repository.findOne({ id: "1234" });
+	it("should return a character by id", function () {
+		req.params.id = "1234";
+		repository.findOne({ id: "1234" });
 
-    findOne(req, res);
+		findOne(req, res);
 
-    // @ts-ignore
-    sinon.assert.calledOnce(res.json);
-    // @ts-ignore
+		// @ts-ignore
+		sinon.assert.calledOnce(res.json);
+		// @ts-ignore
 
-    // en los res.json hay que cambiar el assert xq cambia como el res.send y res.json manejan la data
+		// en los res.json hay que cambiar el assert xq cambia como el res.send y res.json manejan la data
 
-    expect(res.json.firstCall.args[0]).to.deep.equal({
-      data: {
-        name: "Darth Vader",
-        characterClass: "Sith",
-        level: 11,
-        hp: 101,
-        mana: 22,
-        attack: 11,
-        items: ["Lightsaber", "Death Star"],
-        id: "1234",
-      },
-    });
-  });
+		expect(res.json.firstCall.args[0]).to.deep.equal({
+			data: {
+				name: "Darth Vader",
+				characterClass: "Sith",
+				level: 11,
+				hp: 101,
+				mana: 22,
+				attack: 11,
+				items: ["Lightsaber", "Death Star"],
+				id: "1234",
+			},
+		});
+	});
 
-  it("should return 404 if character not found", function () {
-    req.params.id = "0000";
-    repository.findOne({ id: "0000" });
+	it("should return 404 if character not found", function () {
+		req.params.id = "0000";
+		repository.findOne({ id: "0000" });
 
-    findOne(req, res);
+		findOne(req, res);
 
-    // @ts-ignore
-    sinon.assert.calledOnce(res.status);
-    // @ts-ignore
-    sinon.assert.calledWith(res.status, 404);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.send);
-    // @ts-ignore
-    sinon.assert.calledWith(res.send, { message: "Character not found" });
-  });
+		// @ts-ignore
+		sinon.assert.calledOnce(res.status);
+		// @ts-ignore
+		sinon.assert.calledWith(res.status, 404);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.send);
+		// @ts-ignore
+		sinon.assert.calledWith(res.send, { message: "Character not found" });
+	});
 
-  it("should add a new character", function () {
-    req.body.sanitizedInput = {
-      name: "Leia Organa",
-      characterClass: "Rebel Leader",
-      level: 15,
-      hp: 120,
-      mana: 80,
-      attack: 35,
-      items: ["Blaster", "Diplomacy"],
-      id: "5678",
-    };
+	it("should add a new character", function () {
+		req.body.sanitizedInput = {
+			name: "Leia Organa",
+			characterClass: "Rebel Leader",
+			level: 15,
+			hp: 120,
+			mana: 80,
+			attack: 35,
+			items: ["Blaster", "Diplomacy"],
+			id: "5678",
+		};
 
-    add(req, res);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.status);
-    // @ts-ignore
-    sinon.assert.calledWith(res.status, 201);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.send);
-    // @ts-ignore
-    sinon.assert.calledWith(res.send, {
-      message: "Character created",
-      data: {
-        name: "Leia Organa",
-        characterClass: "Rebel Leader",
-        level: 15,
-        hp: 120,
-        mana: 80,
-        attack: 35,
-        items: ["Blaster", "Diplomacy"],
-        id: "5678",
-      },
-    });
-  });
+		add(req, res);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.status);
+		// @ts-ignore
+		sinon.assert.calledWith(res.status, 201);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.send);
+		// @ts-ignore
+		sinon.assert.calledWith(res.send, {
+			message: "Character created",
+			data: {
+				name: "Leia Organa",
+				characterClass: "Rebel Leader",
+				level: 15,
+				hp: 120,
+				mana: 80,
+				attack: 35,
+				items: ["Blaster", "Diplomacy"],
+				id: "5678",
+			},
+		});
+	});
 
-  it("should update an existing character", function () {
-    req.body.sanitizedInput = {
-      name: "Leia Organa",
-      level: 20,
-    };
-    req.params.id = "5678";
+	it("should update an existing character", function () {
+		req.body.sanitizedInput = {
+			name: "Leia Organa",
+			level: 20,
+		};
+		req.params.id = "5678";
 
-    update(req, res);
+		update(req, res);
 
-    // @ts-ignore
-    sinon.assert.calledOnce(res.status);
-    // @ts-ignore
-    sinon.assert.calledWith(res.status, 200);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.send);
-    // @ts-ignore
-    sinon.assert.calledWith(res.send, {
-      message: "Character updated successfully",
-      data: {
-        name: "Leia Organa",
-        characterClass: "Rebel Leader",
-        level: 20,
-        hp: 120,
-        mana: 80,
-        attack: 35,
-        items: ["Blaster", "Diplomacy"],
-        id: "5678",
-      },
-    });
-  });
+		// @ts-ignore
+		sinon.assert.calledOnce(res.status);
+		// @ts-ignore
+		sinon.assert.calledWith(res.status, 200);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.send);
+		// @ts-ignore
+		sinon.assert.calledWith(res.send, {
+			message: "Character updated successfully",
+			data: {
+				name: "Leia Organa",
+				characterClass: "Rebel Leader",
+				level: 20,
+				hp: 120,
+				mana: 80,
+				attack: 35,
+				items: ["Blaster", "Diplomacy"],
+				id: "5678",
+			},
+		});
+	});
 
-  it("should return 404 when updating a non-existent character", function () {
-    req.body.sanitizedInput = {
-      name: "Han Solo",
-      level: 20,
-    };
-    req.params.id = "9999";
+	it("should return 404 when updating a non-existent character", function () {
+		req.body.sanitizedInput = {
+			name: "Han Solo",
+			level: 20,
+		};
+		req.params.id = "9999";
 
-    update(req, res);
+		update(req, res);
 
-    // @ts-ignore
-    sinon.assert.calledOnce(res.status);
-    // @ts-ignore
-    sinon.assert.calledWith(res.status, 404);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.send);
-    // @ts-ignore
-    sinon.assert.calledWith(res.send, { message: "Character not found" });
-  });
+		// @ts-ignore
+		sinon.assert.calledOnce(res.status);
+		// @ts-ignore
+		sinon.assert.calledWith(res.status, 404);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.send);
+		// @ts-ignore
+		sinon.assert.calledWith(res.send, { message: "Character not found" });
+	});
 
-  it("should delete a character by id", function () {
-    req.params.id = "5678";
+	it("should delete a character by id", function () {
+		req.params.id = "5678";
 
-    remove(req, res);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.status);
-    // @ts-ignore
-    sinon.assert.calledWith(res.status, 200);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.send);
-    // @ts-ignore
-    sinon.assert.calledWith(res.send, {
-      message: "Character deleted successfully",
-    });
-  });
+		remove(req, res);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.status);
+		// @ts-ignore
+		sinon.assert.calledWith(res.status, 200);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.send);
+		// @ts-ignore
+		sinon.assert.calledWith(res.send, {
+			message: "Character deleted successfully",
+		});
+	});
 
-  it("should return 404 when deleting a non-existent character", function () {
-    req.params.id = "9999";
+	it("should return 404 when deleting a non-existent character", function () {
+		req.params.id = "9999";
 
-    remove(req, res);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.status);
-    // @ts-ignore
-    sinon.assert.calledWith(res.status, 404);
-    // @ts-ignore
-    sinon.assert.calledOnce(res.send);
-    // @ts-ignore
-    sinon.assert.calledWith(res.send, { message: "Character not found" });
-  });
+		remove(req, res);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.status);
+		// @ts-ignore
+		sinon.assert.calledWith(res.status, 404);
+		// @ts-ignore
+		sinon.assert.calledOnce(res.send);
+		// @ts-ignore
+		sinon.assert.calledWith(res.send, { message: "Character not found" });
+	});
 });
--- technologies/mocha/test/genericos.test.ts
+++ technologies/mocha/test/genericos.test.ts
@@ -3,31 +3,81 @@ import { assert, expect } from "chai";
 // async
 
 function delayedAdd(a: number, b: number) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(a + b);
-    }, 1000);
-  });
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(a + b);
+		}, 1000);
+	});
 }
 
 describe("delayedAdd()", function () {
-  it("should add 1 and 2", async function () {
-    const result = await delayedAdd(1, 2);
-    assert.strictEqual(result, 3);
-  });
+	it("should add 1 and 2", async function () {
+		const result = await delayedAdd(1, 2);
+		assert.strictEqual(result, 3);
+	});
 
-  it("should add 5 and 5", async function () {
-    const result = await delayedAdd(5, 5);
-    assert.strictEqual(result, 10);
-  });
+	it("should add 5 and 5", async function () {
+		const result = await delayedAdd(5, 5);
+		assert.strictEqual(result, 10);
+	});
 
-  it("should add 10 and 20", async function () {
-    const result = await delayedAdd(10, 20);
-    assert.strictEqual(result, 30);
-  });
+	it("should add 10 and 20", async function () {
+		const result = await delayedAdd(10, 20);
+		assert.strictEqual(result, 30);
+	});
 
-  it("should add 0 and 0", async function () {
-    const result = await delayedAdd(0, 0);
-    assert.strictEqual(result, 0);
-  });
+	it("should add 0 and 0", async function () {
+		const result = await delayedAdd(0, 0);
+		assert.strictEqual(result, 0);
+	});
+});
+
+/// Para probar tests dinamicos
+
+function sumToN(n: number) {
+	let sum = 0;
+	for (let i = 0; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
+describe("dynamic()", function () {
+	function add(args: number[]) {
+		return args.reduce((prev, curr) => prev + curr, 0);
+	}
+
+	let startTime: number;
+
+	before(function () {
+		startTime = Date.now();
+	});
+
+	after(function () {
+		const endTime = Date.now();
+		const duration = endTime - startTime;
+		console.log(`Test suite took ${duration}ms to run.`);
+	});
+
+	let tests = [];
+
+	for (let i = 0; i < 7000; i++) {
+		let newElement: { args: number[]; expected: number } = {
+			args: [],
+			expected: sumToN(i + 1),
+		};
+
+		for (let j = 0; j < i + 1; j++) {
+			newElement.args.push(j + 1);
+		}
+
+		tests.push(newElement);
+	}
+
+	tests.forEach(({ args, expected }) => {
+		it(`correctly adds ${args.length} args`, function () {
+			const res = add(args);
+			assert.strictEqual(res, expected);
+		});
+	});
 });
